d34f43669a2dd63840c97188b283dd95d57d393a
Daniel Lucas <stu124145@mail.uni-kiel.de>
2018-06-27 15:22:34 +0200

    Fixes for OS indepdenent use.

--- elkTranGen/src/main/java/elkTranGen/conf/Read.java
+++ elkTranGen/src/main/java/elkTranGen/conf/Read.java
@@ -4,14 +4,15 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.io.File;
 
 /**
  *  @author dalu2104
  */
 public class Read {
-	
+
 	/** Reads a given property from the properties file. Returns the value of the property or null, if property was not found.
-	 * 
+	 *
 	 * @param property
 	 * @return
 	 */
@@ -22,15 +23,17 @@ public class Read {
 		try {
 			//read config file in root directory
 			String current = new java.io.File( "." ).getCanonicalPath();
-			String rootDir = current.substring(0, current.lastIndexOf("\\")+1);
+			File file = new File(current);
+			String rootDir = file.getParentFile().getCanonicalPath();
+
+			input = new FileInputStream(rootDir + System.getProperty("file.separator") +  "config.properties");
 			
-			input = new FileInputStream(rootDir + "config.properties");
 			// load a properties file
 			prop.load(input);
 
 			// get the property value and print it out
 			String returner = prop.getProperty(property);
-			
+
 			return returner;
 
 		} catch (IOException ex) {
